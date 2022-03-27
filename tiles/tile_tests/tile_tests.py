88b55261346e6f137c013a0c6cf0f9b5dbc668ea
Benjamin Trounson <btrounson@outlook.co.nz>
2022-03-28 02:59:31 +1300

    Final Changes

--- tiles/tile_tests/tile_tests.py
+++ tiles/tile_tests/tile_tests.py
@@ -10,7 +10,7 @@ class TileTestCases(unittest.TestCase):
         tile = Tile(TileType.Foyer, 0, 0)
         tile.rotate_tile(RotationDirection.Right)
         expected_index = 1
-        actual_index = tile.get_door_index_from_position(TilePosition.North)
+        actual_index = tile.get_door_index_from_position(TilePosition.North.value)
         self.assertEqual(expected_index, actual_index)  # add assertion here
 
     def test_when_rotate_right_twice_that_door_index_changes(self):
@@ -18,14 +18,14 @@ class TileTestCases(unittest.TestCase):
         tile.rotate_tile(RotationDirection.Right)
         tile.rotate_tile(RotationDirection.Right)
         expected_index = 2
-        actual_index = tile.get_door_index_from_position(TilePosition.North)
+        actual_index = tile.get_door_index_from_position(TilePosition.North.value)
         self.assertEqual(expected_index, actual_index)
 
     def test_when_rotate_left_once_that_door_index_changes(self):
         tile = Tile(TileType.Foyer, 0, 0)
         tile.rotate_tile(RotationDirection.Left)
         expected_index = 3
-        actual_index = tile.get_door_index_from_position(TilePosition.North)
+        actual_index = tile.get_door_index_from_position(TilePosition.North.value)
         self.assertEqual(expected_index, actual_index)
 
     def test_when_rotate_left_twice_that_door_index_changes(self):
@@ -33,7 +33,7 @@ class TileTestCases(unittest.TestCase):
         tile.rotate_tile(RotationDirection.Left)
         tile.rotate_tile(RotationDirection.Left)
         expected_index = 2
-        actual_index = tile.get_door_index_from_position(TilePosition.North)
+        actual_index = tile.get_door_index_from_position(TilePosition.North.value)
         self.assertEqual(expected_index, actual_index)
 
 
